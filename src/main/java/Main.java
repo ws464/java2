492d19e41b86918db981c214e497debca3a4dbea
Sassi <william.sassi@fiserv.com>
2023-10-24 15:43:43 -0400

    Ex1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,9 +6,21 @@ public class Main {
         // ex4();
         // ex5();
     }
-
+    
     private static void ex1() {
-        System.out.println("Todo...");
+        var p1 = new Person();
+        var p2 = new Person("Jon", "Smith");
+        var p3 = new Person("Mary", "Jane", (byte) 12, 123456789);
+
+        System.out.println(p1.speak());
+        System.out.println(p2.speak());
+        System.out.println(p3.speak());
+
+        System.out.println(p1);
+        System.out.println(p2);
+        System.out.println(p3);
+
+        System.out.println("Person count: " + Person.getPersonCount());
     }
 
     private static void ex2() {
